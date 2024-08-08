0379a8855094ed9344255b540ce7e7283c2431e4
maddy <maddyDev@gmail.com>
2024-08-08 11:07:18 +0530

    upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 19;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
